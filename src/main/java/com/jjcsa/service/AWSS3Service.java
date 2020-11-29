4cdcc25839d8b1ea06b7b30f9a0fafc6a9f2bdb8
Tejas Shah <shah.te@northeastern.edu>
2020-11-29 17:56:43 -0500

    Create S3 service to upload community doc from register api

---
+++ src/main/java/com/jjcsa/service/AWSS3Service.java
@@ -0,0 +1,59 @@
+package com.jjcsa.service;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.jjcsa.util.ImageUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.core.parameters.P;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+
+@Slf4j
+@Service
+public class AWSS3Service {
+
+    @Value("${aws.s3.bucketname}")
+    private String bucketName;
+
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    private AmazonS3 s3Client;
+
+    private AmazonS3 getS3Client() {
+        if(s3Client == null) {
+            s3Client = AmazonS3ClientBuilder.standard()
+                    .withRegion(awsRegion)
+                    .build();
+        }
+        return s3Client;
+    }
+
+    public String saveFile(String objectKey, MultipartFile multipartFile) {
+        File file = ImageUtil.convertMultiPartFileToFile(multipartFile);
+        try {
+            getS3Client().putObject(bucketName, objectKey, file);
+        } catch (AmazonServiceException e) {
+            log.error("Unable to save image to s3: " + e.getErrorMessage());
+            return "";
+        }
+        // delete the file created locally
+        file.delete();
+
+        return getS3Client().getUrl(bucketName, objectKey).toString();
+    }
+
+    public void deleteFile(String objectKey) {
+        try {
+            getS3Client().deleteObject(bucketName, objectKey);
+        } catch (AmazonServiceException e) {
+            log.error("Unable to delete image from s3: " + e.getErrorMessage());
+        }
+    }
+}
--- src/main/java/com/jjcsa/service/UserProfileService.java
+++ src/main/java/com/jjcsa/service/UserProfileService.java
@@ -2,8 +2,10 @@ package com.jjcsa.service;
 
 import com.jjcsa.model.UserProfile;
 import com.jjcsa.repository.UserProfileRespository;
+import com.jjcsa.util.ImageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -13,13 +15,27 @@ public class UserProfileService {
 
     @Autowired
     private UserProfileRespository userProfileRepository;
+    @Autowired
+    private AWSS3Service awss3Service;
 
     public UserProfile getUserProfile(UUID id) {
         Optional<UserProfile> user = userProfileRepository.findById(id);
         return null;
     }
 
-    public UserProfile saveUserProfile(UserProfile userProfile) {
+    public UserProfile saveUserProfile(UserProfile userProfile, MultipartFile jainProofDoc) {
+        // Save MultipartFile to S3
+        if(userProfile.getCommunityDocumentURL() != null
+            && !userProfile.getCommunityDocumentURL().isEmpty()) {
+            // Delete file if already present
+            String key = userProfile.getId() + "/COMMUNITY_DOCUMENT."
+                    + userProfile.getCommunityDocumentURL().substring(userProfile.getCommunityDocumentURL().lastIndexOf(".")+1);
+            awss3Service.deleteFile(key);
+        }
+        String fileKey = userProfile.getUserLogin().getId() + "/" + ImageUtil.generateCommunityDocumentName(jainProofDoc);
+        String jainProofDocURL = awss3Service.saveFile(fileKey, jainProofDoc);
+        userProfile.setCommunityDocumentURL(jainProofDocURL);
+
         return userProfileRepository.save(userProfile);
     }
 
