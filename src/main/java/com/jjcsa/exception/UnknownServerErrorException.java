bbe100fd73580dd8e2faf3958f01fb82ce0dc2a4
Tejas Shah <shah.te@northeastern.edu>
2020-12-20 19:18:08 -0500

    Add Profile Picture in the Register API request

--- src/main/java/com/jjcsa/exception/UnknownServerErrorException.java
+++ src/main/java/com/jjcsa/exception/UnknownServerErrorException.java
@@ -2,8 +2,10 @@ package com.jjcsa.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @AllArgsConstructor
+@EqualsAndHashCode(callSuper=false)
 @Data
 public class UnknownServerErrorException extends RuntimeException{
     private String message;
