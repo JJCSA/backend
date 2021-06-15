5896638d31dc745604e2d0b9c14251ea18834ce0
Tejas Shah <tej230@gmail.com>
2021-06-14 23:12:38 -0700

    Unit test for UserProfileService

--- src/main/java/com/jjcsa/service/UserProfileService.java
+++ src/main/java/com/jjcsa/service/UserProfileService.java
@@ -3,17 +3,13 @@ package com.jjcsa.service;
 import com.jjcsa.dto.UserProfile;
 import com.jjcsa.exception.BadRequestException;
 import com.jjcsa.mapper.UserProfileMapper;
-import com.jjcsa.model.Education;
 import com.jjcsa.model.User;
-import com.jjcsa.model.WorkEx;
 import com.jjcsa.repository.EducationRepository;
-import com.jjcsa.repository.UserRepository;
 import com.jjcsa.repository.WorkExRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 @Slf4j
@@ -21,7 +17,6 @@ import java.util.List;
 public class UserProfileService {
 
     private final UserService userService;
-    private final UserRepository userRepository;
     private final EducationRepository educationRepository;
     private final WorkExRepository workExRepository;
     private final UserProfileMapper userProfileMapper;
@@ -37,15 +32,9 @@ public class UserProfileService {
                     ""
             );
 
-//        user.setEducationList(educationRepository.findAllByUser(user));
-//        user.setWorkExperience(workExRepository.findAllByUser(user));
-        List<Education> educationList = educationRepository.findAllByUser(user);
-        List<WorkEx> workExperiences = workExRepository.findAllByUser(user);
+        user.setEducationList(educationRepository.findAllByUser(user));
+        user.setWorkExperience(workExRepository.findAllByUser(user));
 
-        UserProfile userProfile = userProfileMapper.convert(user);
-        userProfileMapper.addEducationAndWorkExperience(userProfile, educationList, workExperiences);
-
-        return userProfile;
-
+        return userProfileMapper.toUserProfile(user);
     }
 }
