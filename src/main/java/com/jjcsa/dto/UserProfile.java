c41e1d27c8710e832c381680d4aead1690ee0cea
Tejas Shah <tej230@gmail.com>
2021-06-14 22:48:47 -0700

    Add mapper unit test

--- src/main/java/com/jjcsa/dto/UserProfile.java
+++ src/main/java/com/jjcsa/dto/UserProfile.java
@@ -5,13 +5,19 @@ import com.jjcsa.model.WorkEx;
 import com.jjcsa.model.enumModel.ContactMethod;
 import com.jjcsa.model.enumModel.UserStatus;
 import com.jjcsa.model.enumModel.VolunteeringInterest;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserProfile {
     private UUID id;
     private String email;
--- src/main/java/com/jjcsa/mapper/UserProfileMapper.java
+++ src/main/java/com/jjcsa/mapper/UserProfileMapper.java
@@ -5,11 +5,23 @@ import com.jjcsa.model.Education;
 import com.jjcsa.model.User;
 import com.jjcsa.model.WorkEx;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface UserProfileMapper {
+
+    @Mappings({
+            @Mapping(source = "user.educationList", target = "education")
+    })
+    UserProfile toUserProfile(User user);
+
+    @Mappings({
+            @Mapping(source = "userProfile.education", target = "educationList")
+    })
+    User toUser(UserProfile userProfile);
 
     UserProfile convert(User user);
 
