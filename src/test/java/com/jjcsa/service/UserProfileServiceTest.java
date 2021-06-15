e7c88e5aa10ed7a81a9f186e5556d04d1479f612
Tejas Shah <tej230@gmail.com>
2021-06-14 23:18:51 -0700

    Invalid case test

--- src/test/java/com/jjcsa/service/UserProfileServiceTest.java
+++ src/test/java/com/jjcsa/service/UserProfileServiceTest.java
@@ -29,8 +29,7 @@ import static org.mockito.Mockito.when;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
