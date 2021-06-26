43fc2dc40237476604d145872f8545de733db9c4
Sanket A. Shah <sanketshah1997@gmail.com>
2021-06-26 00:42:35 -0400

    [JJC_SA-46] Remove Password and userRole from User POJO

--- src/main/java/com/jjcsa/model/User.java
+++ src/main/java/com/jjcsa/model/User.java
@@ -30,12 +30,8 @@ public class User {
 
     private String email;
 
-    @JsonIgnore
-    private String password;
-
-    public User(String email, String password) {
+    public User(String email) {
         this.email = email;
-        this.password = password;
     }
 
     @OneToMany(mappedBy = "user")
@@ -43,10 +39,6 @@ public class User {
 
     @OneToMany(mappedBy = "user")
     private List<WorkEx> workExperience;
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "user_role", columnDefinition = "varchar(45) default User")
-    private UserRole userrole;
 
     @JsonFormat(pattern = "dd/MM/yyyy")
     @Column(name = "last_updated_date", columnDefinition = "varchar(45) default '11/11/1111'")
