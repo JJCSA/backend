ac60f92f57501020e26027657021c5f198ee0928
Darshan Ashwin Dedhia <dedhia.da@husky.neu.edu>
2021-06-26 14:50:42 -0400

    Implemented functionalities for saving, updating and retriving email templates from Database

---
+++ src/main/java/com/jjcsa/model/EmailTemplate.java
@@ -0,0 +1,39 @@
+package com.jjcsa.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "email_templates")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class EmailTemplate {
+    @Id
+    @Column(name="template_id", columnDefinition = "uuid")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int templateId;
+
+    @Column(name="template_name", columnDefinition = "varchar(255) default ''", unique=true)
+    @NotNull
+    private String templateName;
+
+    @Column(name="email_subject", columnDefinition = "varchar(255) default ''")
+    @NotNull
+    private String emailSubject;
+
+    @Column(name="email_body", columnDefinition = "text default ''")
+    @NotNull
+    private String emailBody;
+
+    @Column(name="template_status", columnDefinition = "varchar(1) default 'A'")
+    @NotNull
+    private String templateStatus;
+
+}
