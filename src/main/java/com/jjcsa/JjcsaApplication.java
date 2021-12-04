7502e0e3a9d8da0740cc075f9ac01b33e13a3ba4
Tejas Shah <44346573+shah-tejas@users.noreply.github.com>
2021-12-03 19:39:30 -0800

    paramstore support (#91)

--- src/main/java/com/jjcsa/JjcsaApplication.java
+++ src/main/java/com/jjcsa/JjcsaApplication.java
@@ -1,8 +1,11 @@
 package com.jjcsa;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class JjcsaApplication {
@@ -11,4 +14,14 @@ public class JjcsaApplication {
 		SpringApplication.run(JjcsaApplication.class, args);
 	}
 
+	@RestController
+	class TestController {
+		@Value("${test.variable:local-value}")
+		private String testVariable;
+
+		@GetMapping("/test/param-store")
+		public String getTestVariable() {
+			return testVariable;
+		}
+	}
 }
