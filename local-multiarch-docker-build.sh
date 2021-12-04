dcef986bf03410ece9d65dfe687f3499f7ac0958
Dushyant Dubaria <dushyant.dubaria2010@gmail.com>
2021-12-04 12:55:39 -0500

    Added docker-compose.yaml & Updated Authentication Readme file

--- local-multiarch-docker-build.sh
+++ local-multiarch-docker-build.sh
@@ -13,7 +13,7 @@ echo $COMMIT_HASH
 docker build -t jjcsa:1.0-amd64 --build-arg ARCH=amd64 --build-arg server_port=9080 --build-arg profiles_active=local,docker .
 
 docker tag jjcsa:1.0-amd64 dushyant8858/jjcsa-backend:$COMMIT_HASH-amd64
-docker push dushyant8858/jjcsa-backend:$COMMIT_HASH-amd64
+# docker push dushyant8858/jjcsa-backend:$COMMIT_HASH-amd64
 
 docker tag jjcsa:1.0-amd64 dushyant8858/jjcsa-backend:latest-amd64
 # docker push dushyant8858/jjcsa-backend:latest-amd64
@@ -25,7 +25,7 @@ docker tag jjcsa:1.0-amd64 dushyant8858/jjcsa-backend:latest-amd64
 docker build -t jjcsa:1.0-arm64v8 --build-arg ARCH=arm64v8 --build-arg server_port=9080 --build-arg profiles_active=local,docker .
 
 docker tag jjcsa:1.0-arm64v8 dushyant8858/jjcsa-backend:$COMMIT_HASH-arm64v8
-docker push dushyant8858/jjcsa-backend:$COMMIT_HASH-arm64v8
+# docker push dushyant8858/jjcsa-backend:$COMMIT_HASH-arm64v8
 
 docker tag jjcsa:1.0-arm64v8 dushyant8858/jjcsa-backend:latest-arm64v8
 # docker push dushyant8858/jjcsa-backend:latest-arm64v8
@@ -37,7 +37,7 @@ docker manifest create \
     --amend dushyant8858/jjcsa-backend:$COMMIT_HASH-amd64 \
     --amend dushyant8858/jjcsa-backend:$COMMIT_HASH-arm64v8
 
-docker manifest push dushyant8858/jjcsa-backend:$COMMIT_HASH
+# docker manifest push dushyant8858/jjcsa-backend:$COMMIT_HASH
 
 ##
 docker manifest create \
