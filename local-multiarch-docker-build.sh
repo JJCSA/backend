41974afd6c1e623edea959e45c5bba37a1bb60d3
Dushyant Dubaria <dushyant.dubaria2010@gmail.com>
2021-12-04 12:43:35 -0500

    Added docker-compose.yaml & Updated Authentication Readme file

---
+++ local-multiarch-docker-build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -xe
+
+## AMD64
+# docker build -t jjcsa:1.0-amd64 --build-arg ARCH=amd64 --build-arg server_port=9080 --build-arg profiles_active=local,docker --no-cache .
+
+docker build -t jjcsa:1.0-amd64 --build-arg ARCH=amd64 --build-arg server_port=9080 --build-arg profiles_active=local,docker .
+
+docker tag jjcsa:1.0-amd64 dushyant8858/jjcsa-backend:latest-amd64
+# docker push dushyant8858/jjcsa-backend:latest-amd64
+
+
+## ARM64V8
+# docker build -t jjcsa:1.0-arm64v8 --build-arg ARCH=arm64v8 --build-arg server_port=9080 --build-arg profiles_active=local,docker --no-cache .
+
+docker build -t jjcsa:1.0-arm64v8 --build-arg ARCH=arm64v8 --build-arg server_port=9080 --build-arg profiles_active=local,docker .
+
+docker tag jjcsa:1.0-arm64v8 dushyant8858/jjcsa-backend:latest-arm64v8
+# docker push dushyant8858/jjcsa-backend:latest-arm64v8
+
+
+#####
+
+docker manifest create \
+    dushyant8858/jjcsa-backend:latest \
+    --amend dushyant8858/jjcsa-backend:latest-amd64 \
+    --amend dushyant8858/jjcsa-backend:latest-arm64v8
+
+# docker manifest push dushyant8858/jjcsa-backend:latest
+
+
+
