b000e659445332e58f08a1a592f03c42613d8538
Sanket Shah <sanketshah1997@gmail.com>
2022-02-26 12:10:19 -0500

    "[backend_74] Initial Search API "

--- src/main/java/com/jjcsa/controller/user/UserProfileController.java
+++ src/main/java/com/jjcsa/controller/user/UserProfileController.java
@@ -4,25 +4,15 @@ import com.jjcsa.dto.UserProfile;
 import com.jjcsa.model.User;
 import com.jjcsa.service.UserProfileService;
 import com.jjcsa.service.UserService;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.keycloak.adapters.springsecurity.account.SimpleKeycloakAccount;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.keycloak.representations.AccessToken;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.UUID;
 
 import static java.util.Objects.isNull;
 
