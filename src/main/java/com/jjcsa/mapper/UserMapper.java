2a9cebe5ce3bb744c8ff8cd2d81de5c3033badac
Nishit Gopani <nishitgopani@gmail.com>
2022-06-21 22:26:34 -0400

    Initial comment for getting image from s3

--- src/main/java/com/jjcsa/mapper/UserMapper.java
+++ src/main/java/com/jjcsa/mapper/UserMapper.java
@@ -1,12 +1,14 @@
 package com.jjcsa.mapper;
 
 import com.jjcsa.dto.AddNewUser;
+import com.jjcsa.dto.UserDTO;
 import com.jjcsa.model.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
-import org.mapstruct.NullValueCheckStrategy;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @Mapper(componentModel = "spring")
@@ -17,4 +19,10 @@ public interface UserMapper {
             @Mapping(source = "addNewUser.jainCommunity", target = "communityName")
     })
     User toUserProfile(AddNewUser addNewUser);
+
+    @Mappings({
+            @Mapping(source = "addNewUser.prefMethodOfContact", target = "contactMethod"),
+            @Mapping(source = "addNewUser.jainCommunity", target = "communityName")
+    })
+    List<UserDTO> toUser(List<User> User);
 }
---
+++ src/main/java/com/jjcsa/util/ApplicationConstants.java
@@ -0,0 +1,7 @@
+package com.jjcsa.util;
+
+public class ApplicationConstants {
+    public static final String FILE_TYPE_PROFILE = "PROFILE_PICTURE.jpeg";
+    public static final String FILE_TYPE_COMMUNITY = "COMMUNITY.jpeg";
+
+}
