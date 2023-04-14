e76f46d5f669865c0148db0bb7854057ebd61042
Nishit Gopani <nishitgopani@gmail.com>
2023-04-14 00:00:38 -0400

    created new api endpoint for contact us captcha verification

---
+++ src/main/java/com/jjcsa/util/RecaptchaResponse.java
@@ -0,0 +1,26 @@
+package com.jjcsa.util;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class RecaptchaResponse {
+    @JsonProperty("success")
+    private boolean success;
+
+    @JsonProperty("challenge_ts")
+    private String challengeTimestamp;
+
+    @JsonProperty("hostname")
+    private String hostname;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getChallengeTimestamp() {
+        return challengeTimestamp;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+}
