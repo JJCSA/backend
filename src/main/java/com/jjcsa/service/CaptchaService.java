36fcaea6fb6ce6d6aea1ee97aa9c165e1f1342f5
Tejas Shah <tej230@gmail.com>
2023-06-26 21:17:05 -0700

    change captcha service

--- src/main/java/com/jjcsa/service/CaptchaService.java
+++ src/main/java/com/jjcsa/service/CaptchaService.java
@@ -1,19 +1,19 @@
 package com.jjcsa.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jjcsa.exception.BadRequestException;
 import com.jjcsa.dto.RecaptchaResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
@@ -32,38 +32,9 @@ public class CaptchaService {
 
     private static Pattern RESPONSE_PATTERN = Pattern.compile("[A-Za-z0-9_-]+");
 
-    public boolean verifyCaptcha(String recaptchaToken) throws IOException {
-        log.info("Inside verifyCaptcha");
-        String urlParameters = "secret=" + URLEncoder.encode(reCaptchaSecret, "UTF-8") +
-                "&response=" + URLEncoder.encode(recaptchaToken, "UTF-8");
-
-        URL url = new URL("https://www.google.com/recaptcha/api/siteverify");
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("POST");
-        conn.setDoOutput(true);
-
-        conn.getOutputStream().write(urlParameters.getBytes("UTF-8"));
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String inputLine;
-        StringBuilder responseBuilder = new StringBuilder();
-
-        while ((inputLine = in.readLine()) != null) {
-            responseBuilder.append(inputLine);
-        }
-        in.close();
-
-        String responseString = responseBuilder.toString();
-        log.info("Inside response {}",responseString);
-        ObjectMapper objectMapper = new ObjectMapper();
-        RecaptchaResponse recaptchaResponse = objectMapper.readValue(responseString, RecaptchaResponse.class);
-
-        return recaptchaResponse.isSuccess();
-    }
-
     public Boolean validateCaptcha(String token, String clientIp) {
         if(!responseSanityCheck(token)) {
-            throw new BadRequestException("Response contains invalid characters", "", "", "", "");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Response contains invalid characters");
         }
 
         URI verifyUri = URI.create(String.format(
