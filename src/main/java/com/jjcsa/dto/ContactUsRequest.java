2c968d41197a69c8bb805714ee5dc3efdefda7f1
Tejas Shah <tej230@gmail.com>
2023-06-26 21:19:53 -0700

    Add request params

--- src/main/java/com/jjcsa/dto/ContactUsRequest.java
+++ src/main/java/com/jjcsa/dto/ContactUsRequest.java
@@ -2,5 +2,8 @@ package com.jjcsa.dto;
 
 import javax.validation.constraints.NotBlank;
 
-public record ContactUsRequest(@NotBlank(message = "token must not be null!") String captchaToken) {
+public record ContactUsRequest(@NotBlank(message = "token must not be null!") String captchaToken,
+                               @NotBlank(message = "please enter your name") String name,
+                               @NotBlank(message = "please enter a valid email") String email,
+                               @NotBlank(message = "please enter a valid message") String message) {
 }
