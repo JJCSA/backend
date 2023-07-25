62eabd18a60ed27050039a7a129e993db2de6e15
Sanket Shah <sanketshah1997@gmail.com>
2023-07-24 23:42:47 -0400

    "Changes on search and some configs"

--- src/main/java/com/jjcsa/repository/JJCSearchRepository.java
+++ src/main/java/com/jjcsa/repository/JJCSearchRepository.java
@@ -4,6 +4,9 @@ import com.jjcsa.model.JJCSearch;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface JJCSearchRepository extends JpaRepository<JJCSearch, String> {
+    List<JJCSearch> findAllByUserIdNot(String userId);
 }
