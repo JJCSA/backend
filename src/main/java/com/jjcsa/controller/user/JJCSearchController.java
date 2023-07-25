96087dabd5780bb8a151e2340db3e96709b2b03e
Sanket Shah <sanketshah1997@gmail.com>
2023-07-24 23:42:47 -0400

    "Changes on search and some configs"

--- src/main/java/com/jjcsa/controller/user/JJCSearchController.java
+++ src/main/java/com/jjcsa/controller/user/JJCSearchController.java
@@ -1,11 +1,13 @@
 package com.jjcsa.controller.user;
 
 import com.jjcsa.dto.JJCSearchDTO;
-import com.jjcsa.model.JJCSearch;
 import com.jjcsa.service.JJCSearchService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.keycloak.adapters.springsecurity.account.SimpleKeycloakAccount;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.keycloak.representations.AccessToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,9 +24,11 @@ public class JJCSearchController {
     private final JJCSearchService jjcSearchService;
 
     @GetMapping
-    public List<JJCSearchDTO> searchJJCUsers(){
-        log.info("Invoking JJC Search!");
-        return jjcSearchService.invokeJJCSearch();
+    public List<JJCSearchDTO> searchJJCUsers(KeycloakAuthenticationToken authenticationToken) {
+        SimpleKeycloakAccount account = (SimpleKeycloakAccount) authenticationToken.getDetails();
+        AccessToken token = account.getKeycloakSecurityContext().getToken();
+        log.info("Invoking JJC Search for user:{}", token.getSubject());
+        return jjcSearchService.invokeJJCSearch(token.getSubject());
     }
 
 }
