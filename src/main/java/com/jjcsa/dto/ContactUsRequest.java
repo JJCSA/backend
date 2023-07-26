d0c477a90c8ead4e53ac4d2839072ab8219f21c9
Tejas Shah <44346573+shah-tejas@users.noreply.github.com>
2023-07-25 20:45:36 -0700

    change record to class (#134)

--- src/main/java/com/jjcsa/dto/ContactUsRequest.java
+++ src/main/java/com/jjcsa/dto/ContactUsRequest.java
@@ -1,9 +1,24 @@
 package com.jjcsa.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.validation.constraints.NotBlank;
 
-public record ContactUsRequest(@NotBlank(message = "token must not be null!") String captchaToken,
-                               @NotBlank(message = "please enter your name") String name,
-                               @NotBlank(message = "please enter a valid email") String email,
-                               @NotBlank(message = "please enter a valid message") String message) {
+@Getter
+@Setter
+public class ContactUsRequest {
+
+    @NotBlank(message = "token must not be null!")
+    private String captchaToken;
+
+    @NotBlank(message = "please enter your name")
+    private String name;
+
+    @NotBlank(message = "please enter a valid email")
+    private String email;
+
+    @NotBlank(message = "please enter a valid message")
+    private String message;
+
 }
